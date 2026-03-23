e7d6f737013ff5e3403c41c9a1eaaa8140eed36b
Lucas Brembati Weizenmann <lucas8bitbw@gmail.com>
2026-03-23 12:21:44 -0400

    Add files via upload

--- arvore_binaria.c
+++ arvore_binaria.c
@@ -4,7 +4,7 @@
 typedef struct No
 {
     int dado;
-    struct No *esqueda, *direita, *raiz;
+    struct No *esquerda, *direita, *raiz;
 } No;
 
 typedef struct
@@ -12,12 +12,16 @@ typedef struct
     No *raiz;
 } Avb;
 
+void inserir_esquerda();
+
+void inserir_direita();
+
 void inserir_novo_no(Avb *arv, int valor) {
     if(arv->raiz == NULL) {
         No *novoNo = (No*)malloc(sizeof(No));
         novoNo->dado = valor;
         novoNo->esquerda = NULL;
-        novoNo->dreita = NULL;
+        novoNo->direita = NULL;
         arv->raiz = novoNo;
     } else {
         if(valor < arv->raiz->dado) {
@@ -48,8 +52,8 @@ void inserir_esquerda(No *no, int valor) {
     }
 }
 
-void inserir_direita(No *no, valor) {
-    if(no->direia == NULL) {
+void inserir_direita(No *no, int valor) {
+    if(no->direita == NULL) {
         No *novoNo;
         novoNo = (No*)malloc(sizeof(No));
         novoNo->dado = valor;
@@ -69,7 +73,7 @@ void inserir_direita(No *no, valor) {
 void imprimir(No *raiz) {
     if(raiz != NULL) {
         imprimir(raiz->esquerda);
-        print("%d",raiz->dado);
+        printf("%d",raiz->dado);
         imprimir(raiz->direita);
     }
 }
@@ -100,12 +104,12 @@ int tamanho(No *raiz) {
     }
 }
 
-int buscar(No *raiz, int chave) {
+No* buscar(No *raiz, int chave) {
     if(raiz == NULL) {
-        return -1;
+        return NULL;
     } else {
         if(chave == raiz->dado) {
-            return *raiz;
+            return raiz;
         } else {
             if(chave < raiz->dado) {
                 return buscar(raiz->esquerda, chave);
@@ -114,4 +118,49 @@ int buscar(No *raiz, int chave) {
             }
         }
     }
+}
+
+No* remover_elemento(No *raiz, int valor) {
+    if (raiz == NULL) {
+        return NULL;
+    }
+    
+    if (valor < raiz->dado) {
+        raiz->esquerda = remover_elemento(raiz->esquerda, valor);
+    } else if (valor > raiz->dado) {
+        raiz->direita = remover_elemento(raiz->direita, valor);
+    } else {
+        if (raiz->direita == NULL && raiz->esquerda == NULL) {
+            free(raiz);
+            return NULL;
+        }
+
+        if (raiz->esquerda == NULL) {
+            No* temp = raiz->direita;
+            free(raiz);
+            return temp;
+        }
+
+        if (raiz->direita == NULL) {
+            No* temp = raiz->esquerda;
+            free(raiz);
+            return temp;
+        }
+
+        No* substituto = raiz->esquerda;
+
+        while (substituto->direita != NULL) {
+            substituto = substituto->direita;
+        }
+
+        raiz->dado = substituto->dado;
+
+        raiz->esquerda = remover_elemento(raiz->esquerda, substituto->dado);
+    }
+
+    return raiz;
+}
+
+int main() {
+    return 0;
 }
